7366a1d71b77810ba3f414936db944de516de917
Raphael Onunkwor <raphael.onun@gmail.com>
2025-08-28 08:22:04 +0100

    update Redis configuration: add REDIS_ADDR to .env.example and refactor ConnectRedis to use Getenv for address retrieval

--- internal/config/redis.go
+++ internal/config/redis.go
@@ -3,7 +3,6 @@ package config
 import (
 	"context"
 	"log"
-	"os"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -11,13 +10,16 @@ import (
 var Redis *redis.Client
 
 func ConnectRedis() {
+	Addr, err := Getenv("REDIS_ADDR")
+	if err != nil {
+		log.Fatalf("environment variable REDIS_ADDR not set: %v", err)
+	}
 	Redis = redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_ADDR"),
-		Password: os.Getenv("REDIS_PASSWORD"),
-		DB:       0,
+		Addr: Addr,
+		DB:   0,
 	})
 
-	_, err := Redis.Ping(context.Background()).Result()
+	_, err = Redis.Ping(context.Background()).Result()
 	if err != nil {
 		log.Fatalf("failed to connect to redis: %v", err)
 	}
